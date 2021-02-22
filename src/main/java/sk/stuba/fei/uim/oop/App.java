94014f613c2083c2f4c9666ef97b6a1ce536d1ce
NajselIgor <xnajsel@stuba.sk>
2021-02-22 15:56:15 +0100

    Rajd nav

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,4 +1,11 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
+    public static void main(String[] argumentyZKonzoly) {
+        System.out.println("Hello world!");
+        char znak = 97;
+        String st[] = {"skap", "hned"};
+        znak = st[1].charAt(1);
+        System.out.print(znak);
+    }
 }
